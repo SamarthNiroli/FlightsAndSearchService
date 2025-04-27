4cab3747acb2625ad1da1bd1f5a1addd1d0c497a
SamarthNiroli <sniroli137@gmail.com>
2025-04-27 16:49:20 +0530

    Completed city-repository

--- src/index.js
+++ src/index.js
@@ -1,19 +1,23 @@
 const express = require("express");
 const bodyparser = require("body-parser");
 
-
 const { PORT } = require("./config/ServerConfig.js");
+const CityRepository = require("./repository/city-repository.js");
 
-
-const sertupAndStartServer = async () => {
-  //create a express object
+const setupAndStartServer = async () => {
+  // create an express object
   const app = express();
 
   app.use(bodyparser.json());
   app.use(bodyparser.urlencoded({ extended: true }));
+
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
+    // const repo = new CityRepository();
+    // // Use await to ensure that the city is created asynchronously
+    // repo.createCity({ name: "New Delhi" })
+
   });
 };
 
-sertupAndStartServer();
+setupAndStartServer();
---
+++ src/repository/city-repository.js
@@ -0,0 +1,55 @@
+const { City } = require("../models/city");
+
+class CityRepository {
+  async createCity({ name }) {
+    try {
+      const city = await City.create({
+        name: name,
+      });
+      return city;
+    } catch (error) {
+        console.error("Error creating city:", error);
+        throw {error};
+    }
+  }
+
+  async deleteCity(cityId) {
+    try {
+      const city = await City.destroy({
+        where: {
+          id: cityId,
+        },
+      });
+      return true;
+    } catch (error) {
+      console.error("Error deleting city:", error);
+      throw {error};
+    }
+  }
+
+  async updateCity(cityId, data) {
+    try {
+      const city = await City.update(data, {
+        where: {
+          id: cityId,
+        },
+      });
+      return city;
+    } catch (error) {
+      console.error("Error updating city:", error);
+      throw {error};
+    }
+  }
+
+  async getCity(cityId) {
+    try {
+      const city = await City.findByPk(cityId);
+      return city;
+    } catch (error) {
+      console.error("Error fetching city:", error);
+      throw {error};
+    }
+  }
+}
+
+module.exports = CityRepository;
